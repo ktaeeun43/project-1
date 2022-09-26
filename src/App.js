16edc449b62317036292aa994e39e5abd939bcc3
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-26 15:03:06 +0900

    nav 수정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./layout/Header";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Life from "./pages/life/Life";
+import Market from "./pages/market/Market";
+import News from "./pages/news/News";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <RecoilRoot>
-        <Header />
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/life" element={<Life />}></Route>
+            <Route path="/market" element={<Market />}></Route>
+            <Route path="/news" element={<News />}></Route>
+          </Routes>
+        </BrowserRouter>
       </RecoilRoot>
     </>
   );
